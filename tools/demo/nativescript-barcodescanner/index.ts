eec45b8a4349eedeea6495ed178992688963cff9
Eduardo Speroni <edusperoni@gmail.com>
2022-09-16 14:23:55 -0300

    chore: update barcode scanner demo

--- tools/demo/nativescript-barcodescanner/index.ts
+++ tools/demo/nativescript-barcodescanner/index.ts
@@ -2,7 +2,30 @@ import { DemoSharedBase } from '../utils';
 import { BarcodeScanner } from '@valor/nativescript-barcodescanner';
 
 export class DemoSharedNativescriptBarcodescanner extends DemoSharedBase {
-  testIt() {
+  async testIt() {
     console.log('test nativescript-barcodescanner!');
+    const scanner = new BarcodeScanner();
+
+    const available = await scanner.available();
+    console.log('IsAvailable', available);
+
+    if (available) {
+      let hasPermission = await scanner.hasCameraPermission();
+      console.log('hasPermission', hasPermission);
+      if (!hasPermission) {
+        try {
+          await scanner.requestCameraPermission();
+          hasPermission = true;
+          console.log('got permission!');
+        } catch (e) {
+          console.log('Failed getting permission', e);
+          hasPermission = false;
+        }
+      }
+      if (hasPermission) {
+        const result = await scanner.scan({});
+        console.log('scanned', result);
+      }
+    }
   }
 }
