c27fec3aa50967759caa5eb53cc4f33e52661135
Eduardo Speroni <edusperoni@gmail.com>
2026-02-13 10:24:06 -0300

    fix: missing type

--- packages/nativescript-in-app-review/references.d.ts
+++ packages/nativescript-in-app-review/references.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../../references.d.ts" />
 /// <reference path="./types/play-review.d.ts" />
 /// <reference path="./types/play-services-tasks.d.ts" />
+/// <reference path="./types/objc!NSCAppStore.d.ts" />
